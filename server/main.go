317ad6e3d1ec2a32f9dd4ecc4b77f94bc120d17c
anterabyte <dragomon27@gmail.com>
2025-02-26 15:56:34 +0530

    Added new protobuf files and server file

---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/anterabyte/gRPC-tutorial/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	name := req.GetName()
+	message := fmt.Sprintf("Hello, %s!", name)
+	return &pb.HelloResponse{Message: message}, nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+
+		log.Fatalf("Failed to listen, %v",err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := s.Serve(lis)  ; err != nil {
+
+		log.Fatalf("Failed to serve, %v",err)
+	}
+}
